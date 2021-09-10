e83bdbe68c6499eafeeaf7746ba0b172028656ea
den.2k6 <den.2k6aug17@gmail.com>
2021-09-11 07:36:07 +0900

    get parameters from command line

---
+++ test_param.js
@@ -0,0 +1,20 @@
+
+// get command line parameters
+
+const params = process.argv;
+
+for(let i = 0; i < params.length; i++) {
+    console.log(params[i])
+}
+
+params.forEach(param =>{
+    console.log(param)
+})
+
+for (let param of params) {
+    console.log(param)
+}
+
+for (let param in params) {
+    console.log(params[param])
+}
